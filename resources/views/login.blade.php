4941109cf1e3c78afdada2202618a7a114374fa5
Qonitaaa832 <qonita.azizah@student.pradita.ac.id>
2023-11-05 01:32:50 +0700

    UPDATE: Tampilan Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,10 +7,10 @@
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <title>Login</title>
+    <!-- CSS -->
     <style>
         body {
             background-color: #fff; /* Warna latar belakang putih */
-           
         }
     
         .container {
