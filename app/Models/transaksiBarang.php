6c731df4c82edb7e46406ec949d9e18668f9591a
mahezarya <mahezarya23@gmail.com>
2023-11-07 00:26:49 +0700

    FEAT : Variable to table transaction barang

---
+++ app/Models/transaksiBarang.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class transaksiBarang extends Model
+{
+    use HasFactory;
+
+    protected $table = 't_barang';
+}
