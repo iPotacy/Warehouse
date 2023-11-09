658466da3488e5a6d92848863d98d5551f566cde
Qonitaaa832 <qonita.azizah@student.pradita.ac.id>
2023-11-09 08:02:05 +0700

    Tolak

--- resources/views/multiUser.blade.php
+++ resources/views/multiUser.blade.php
@@ -7,6 +7,7 @@
   <title>Admin</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href=" {{ asset ('asset/style.css') }}" rel="stylesheet">
 </head>
 <body>
 
@@ -17,17 +18,41 @@
   {{-- Admin Page --}}
   @if (Auth::user()->role == 'admin')
   {{-- Navbar --}}
-  <div class="bg-warning">
+  <div class="bg-dark">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+          <nav class="navbar navbar-expand-lg navbar-light bg-dark ">
             <div class="container-fluid">
-              <a class="navbar-brand active text-uppercase fw-semibold" href="#">Warehouse</a>
+              <a class="navbar-brand active text-uppercase fw-semibold text-warning" href="#">Warehouse</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <div class="dropdown d-flex justify-content-end mt-3">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent" style="padding: 10px;">
+                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <button type="button" class="btn btn-warning dropdown-toggle" data-bs-toggle="dropdown">
+                  Dropdown 
+                </button>
+             
+                <ul class="dropdown-menu">
+                  <li><a href="" class="dropdown-item">Input Barang</a></li>
+                  <li><a class="dropdown-item" href="#">Input Status</a></li>
+                  <li><a class="dropdown-item" href="#">add Transaction</a></li>
+                </ul>
+              </ul>
+            </div>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent" style="padding: 10px;">
+                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item text-danger">
+                      <a class="btn btn-sm btn-danger active text-uppercase fw-semibold" href="/logout" aria-current="page">
+                          Logout
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              {{--<div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                   <li class="nav-item text-danger">
                     <a class="btn btn-sm btn-danger active text-uppercase fw-semibold" href="/logout" aria-current="page">
@@ -35,7 +60,7 @@
                     </a>
                   </li>
                 </ul>
-              </div>
+              </div>--}}
             </div>
           </nav>
         </div>
@@ -58,7 +83,7 @@
 
         <div class="table-responsive my-3">
           <table class="table table-bordered table-responsive table-hover table-striped table-sm table-md table-lg">
-            <thead class="bg-warning">
+            <thead class="bg-dark text-warning">
               <tr>
                 <th class="text-center" style="white-space: nowrap;">No</th>
                 <th class="text-center" style="white-space: nowrap;">Jenis Barang</th>
