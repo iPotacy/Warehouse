f9164dd10721292b88b05b22051c96460b8f2295
mahezarya <mahezarya23@gmail.com>
2023-11-27 13:04:04 +0700

    UPDATE : DataTables

--- resources/views/home/sidebar.blade.php
+++ resources/views/home/sidebar.blade.php
@@ -74,15 +74,6 @@
         <div data-i18n="Analytics">Dashboard</div>
       </a>
     </li>
-    @endif
-    @if ( Auth::user()->role == 'user')  
-    <li class="menu-item">
-      <a href="{{ url('/user') }}" class="menu-link">
-        <i class="menu-icon tf-icons bx bx-home-circle"></i>
-        <div data-i18n="Analytics">Dashboard</div>
-      </a>
-    </li>
-    @endif
     <!-- Feature -->
     <li class="menu-header small text-uppercase"><span class="menu-header-text">Feature</span></li>
     <!-- Cards -->
@@ -98,12 +89,38 @@
         <div data-i18n="Basic">Items</div>
       </a>
     </li>
+    <!-- Feature -->
     <li class="menu-header small text-uppercase"><span class="menu-header-text">Data</span></li>
+    <!-- Cards -->
     <li class="menu-item">
       <a href="{{ url('/users') }}" class="menu-link">
         <i class="menu-icon tf-icons bx bx-user"></i>
         <div data-i18n="Basic">User</div>
       </a>
     </li>
+    @endif
+    @if ( Auth::user()->role == 'user')  
+    <li class="menu-item">
+      <a href="{{ url('/user') }}" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-home-circle"></i>
+        <div data-i18n="Analytics">Dashboard</div>
+      </a>
+    </li>
+    <!-- Feature -->
+    <li class="menu-header small text-uppercase"><span class="menu-header-text">Feature</span></li>
+    <!-- Cards -->
+    <li class="menu-item">
+      <a href="{{ url('/record') }}" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-line-chart"></i>
+        <div data-i18n="Basic">Record</div>
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="{{ url('/view') }}" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-cabinet"></i>
+        <div data-i18n="Basic">Items</div>
+      </a>
+    </li>
+    @endif
   </ul>
 </aside>
