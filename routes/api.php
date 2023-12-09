738e78156222d5cd3ac06b384d27a8ac93157dd9
NataNaraNPS <natanaraps@gmail.com>
2023-12-09 18:52:16 +0800

    FEAT : API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(["auth:sanctum"])->group(function () {
+    Route::get('/transaksi', [AdminController::class, 'index']);
+    Route::post('/transaksi-create', [AdminController::class,'store']);
+    Route::put('/user/{id}', [AdminController::class, 'update']);
+    Route::delete('/user/{id}', [AdminController::class, 'destroy']);
+    Route::post('/register',[AdminController::class, 'register']);
+});
+
+//auth user rest api
+Route::post('/login',[AuthController::class, 'login']);
