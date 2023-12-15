55523058fd801e59094024384581dabd960458ee
NataNaraNPS <natanaraps@gmail.com>
2023-12-15 23:45:48 +0800

    UPDATE: EXPORT EXCEL & CHANGE POSITION RESET BUTTON

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Admin Warehouse</title>
+  <title>Warehouse</title>
 
   <!-- Favicon -->
   <link rel="icon" type="image/x-icon" href="{{ asset('backend/assets/img/favicon/favicon.ico') }}" />
